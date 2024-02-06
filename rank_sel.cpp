4c1bd97bd6ca2b54e56f36dfe807998fef0805fb
Piotr Beling <piotr.beling@wmii.uni.lodz.pl>
2024-02-06 10:41:32 +0100

    configuration can be given as cmd. params.

--- rank_sel.cpp
+++ rank_sel.cpp
@@ -1,14 +1,17 @@
 #include <cstddef>
 #include <cstdint>
+#include <ostream>
 #include <sdsl/bit_vectors.hpp>
 #include <iostream>
 #include <chrono>
+#include <string>
 
 using namespace std;
 using namespace sdsl;
 
-const std::uint64_t SEED = 1234;
-const std::size_t QUERIES = 1000000;
+std::uint64_t SEED = 1234;
+std::size_t QUERIES = 1000000;
+chrono::seconds TIME_PER_TEST = 5s;
 
 template <class T>
 inline void black_box(T&& datum) {
@@ -24,15 +27,15 @@ uint64_t xor_shift64(uint64_t& seed) {
 
 template<class T>
 void print_result(const char* label, const bit_vector& b, const T& sth, double time) {
-    cout<< label << ":  space overhead " << size_in_bytes(sth) * 100 / size_in_bytes(b) << "\%  time/query " << time << "ns" << endl;
+    cout<< label << ":  space overhead " << size_in_bytes(sth) * 100.0 / size_in_bytes(b) << "\%  time/query " << time << "ns" << endl;
 }
 
-std::vector<std::size_t> rand_queries(std::size_t query_universe) {
+std::vector<std::size_t> rand_queries(std::size_t query_universe, uint64_t from = 0) {
     std::vector<std::size_t> result;
     result.reserve(QUERIES);
     auto seed = SEED;
     for (std::size_t i = 0; i < QUERIES; ++i) {
-        result.push_back(xor_shift64(seed) % query_universe);
+        result.push_back(xor_shift64(seed) % query_universe + from);
     }
     return result;
 }
@@ -41,7 +44,7 @@ template<class F>
 inline double measure(F f) {   // warm + measure; result is in ns
     unsigned iters = 1;
     auto start = chrono::steady_clock::now();
-    while (chrono::steady_clock::now() - start < 5s) {
+    while (chrono::steady_clock::now() - start < TIME_PER_TEST) {
         f();
         iters += 1;
     }
@@ -68,8 +71,28 @@ void benchmark_select(const char* label, const bit_vector& b, const ss& sb, cons
 
 int main(int argc, char *argv[]) 
 {
-    uint64_t universe = 10000000000;
-    uint64_t num = universe / 10;
+    uint64_t universe = 1000000000;
+    uint64_t num = universe / 2;
+
+    if (argc > 1) {
+        if (argc == 2 || argc > 6) {
+            std::cout << "Usage: " << argv[0] << " [bitvec_length number_of_ones [time_per_test_sec [query_num [rand_seed]]]]" << std::endl;
+            return 0;
+        }
+        universe = stoull(argv[1]);
+        num = stoull(argv[2]);
+        if (argc > 3) {
+            TIME_PER_TEST = chrono::seconds(stoull(argv[3]));
+            if (argc > 4) {
+                QUERIES = stoull(argv[4]);
+                if (argc > 4) SEED = stoull(argv[5]);
+            }
+        }
+    }
+
+    std::cout << "Time per warm up and test " << TIME_PER_TEST.count()
+         << "s, number of random queries " << QUERIES
+         << ", random seed " << SEED << "." << std::endl;
     std::cout << "The bit vector consists of " << universe << " bits, including " << num << " ones." << std::endl;
 
     bit_vector b(universe, 0);
@@ -88,6 +111,6 @@ int main(int argc, char *argv[])
         benchmark_rank("rank_v", b, rank_support_v<>(&b), rank_queries);
         benchmark_rank("rank_v5", b, rank_support_v5<>(&b), rank_queries);
     }
-    benchmark_select("select1 mcl", b, select_support_mcl<1>(&b), rand_queries(num));
-    benchmark_select("select0 mcl", b, select_support_mcl<0>(&b), rand_queries(universe-num));
+    benchmark_select("select1 mcl", b, select_support_mcl<1>(&b), rand_queries(num, 1));
+    benchmark_select("select0 mcl", b, select_support_mcl<0>(&b), rand_queries(universe-num, 1));
 }
